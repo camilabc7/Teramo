5ea4a55ef4500e28873e9c4a1bc5c967f5cb5b4b
Camila Barona <cbaronac@eafit.edu.co>
2023-11-06 08:27:51 -0500

    pagina renderizada

--- src/app/modules/service/service-page.component.ts
+++ src/app/modules/service/service-page.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-service-page',
   templateUrl: './service-page.component.html',
-  styleUrls: ['./service-page.component.css']
+  styleUrls: ['./service-page.component.css',]
 })
 export class ServicePageComponent {
 
